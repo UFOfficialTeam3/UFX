7038907baedc9f09752a7a7ef1d637596b93b7c8
Wilson Staley <wilstaley@gmail.com>
2018-10-26 10:00:09 -0400

    Initial Homepage setup
    Made a homepage skeleton.  Linked angularJS

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,5 +1,8 @@
 /* register the modules the application depends upon here*/
-angular.module();
+// angular.module();
 
 /* register the application and inject all the necessary dependencies */
-const app = angular.module();
+const app = angular.module('UFX', []);
+app.controller("ufxCtrl", function($scope) {
+    $scope.a = [1, 2, 3];
+});
