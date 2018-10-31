652f86a19c1524686a39674165f69f01dfdddeca
unknown <apaparazzi0329@ufl.edu>
2018-10-31 11:01:35 -0400

    Default map in place

--- client/src/js/controllers/mapController.js
+++ client/src/js/controllers/mapController.js
@@ -1,22 +1,29 @@
-angular.module('listings').controller('mapController', ['$scope', 'Listings', 
-  function($scope, Listings) {
-    
+
     /**The Controller makes functions available to be called from the html and
      * updates the view after changes are made to it
      * Add functions that will be used by the website to make calls to the appropriate Factories
     */
 
-    $scope.showMap = function() {
+   mapboxgl.accessToken = 'pk.eyJ1IjoiYXBhcGFyYXp6aTAzMjkiLCJhIjoiY2pudXVoYW9kMThrZDN2bnRxcHE3MzZpbSJ9.3To4lk5RKG_8YBaMKZC4mA';
+   var map = new mapboxgl.Map({
+     container: 'map',
+     style: 'mapbox://styles/mapbox/streets-v9',
+     center: [-82.347950, 29.646908],
+     zoom: 13
+   });
+   
+
+    showMap = function() {
       /**TODO
        * Calls the mapFactory to load the map onto the screen
        */
+      
+
+
     }
 
-    $scope.locationConfirm = function(locationObject) {
+    locationConfirm = function(locationObject) {
       /**TODO
        * Calls the mapController to display approved meeting location
        */
     }
-
-  }
-]);
