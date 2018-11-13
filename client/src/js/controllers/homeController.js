d6789efb64e052e1d770c93e26d7db382e0a2ede
Wilson Staley <wilstaley@gmail.com>
2018-11-13 11:53:51 -0500

    Linked Pages Together

--- client/src/js/controllers/homeController.js
+++ client/src/js/controllers/homeController.js
@@ -5,6 +5,26 @@ app.controller('homeController', ['$scope', 'listingsFactory',
     var vm = this;
 
     console.log("Hello from home controller!")
+    vm.listings = {};
+
+    vm.init = function() {
+      listingsFactory.getAll()
+        .then(
+          function(response) {
+            // if http request called successfully
+            //console.log("getAll response", response);
+            vm.listings = response.data;
+            console.log(vm.listings);
+            
+
+          }, function(error) {
+            // if there was an error with the http request
+            console.log("getAll error", error);
+          })
+    }
+
+    // initialize controller and home page
+    vm.init()
 
     
     
--- server/controllers/user.server.controller.js
+++ server/controllers/user.server.controller.js
@@ -2,7 +2,7 @@
 /* Dependencies */
 // connect to database (technically)
 const db = require('../config/config.js');
-const fs = require('fs'); 
+const fs = require('fs');
 /*
   In this file, you should use SQL queries in order to retrieve/add/remove/update Free&4Sale listings.
   On an error you should send a 404 status code, as well as the error message.
@@ -19,9 +19,9 @@ const fs = require('fs');
  */
 
 /** Gets a user by id */
-exports.userByID = function(request, response) {
-    
-      const userID = request.query.id;
+exports.userByID = function(req, response) {
+
+      const userID = req.query.id;
 
       db.query('SELECT * FROM users WHERE uid = $1', [userID], (err, res) => {
         if (err) {
@@ -38,11 +38,11 @@ exports.userByID = function(request, response) {
 };
 
 /** Updates user info */
-exports.update = function(request, response) {
-      const userID = request.body.userID;
+exports.update = function(req, response) {
+      const userID = req.body.userID;
       const f_name = 'Rick';
 
-      db.query('UPDATE users SET f_name=$2 WHERE uid=$1 RETURNING *', [userID, f_name], 
+      db.query('UPDATE users SET f_name=$2 WHERE uid=$1 RETURNING *', [userID, f_name],
       (err, res) => {
         if (err) {
           res.status(404)        // HTTP status 404: Not Found
@@ -55,26 +55,27 @@ exports.update = function(request, response) {
       })
 };
 
-exports.addPic= function(req, res) {
+exports.addPic= function(req, response) {
   fs.readFile('C:\\Users\\paul\\Pictures\\pic2.jpg', (err, imgData) => {
     // inserting data into column 'img' of type 'bytea':
-    db.query('INSERT INTO pictures(pid, picture) VALUES(2 , $1)', [imgData], 
-    (err,response)=> {
+    db.query('INSERT INTO pictures(pid, picture) VALUES(2 , $1)', [imgData],
+    (err,res)=> {
           if(err){
             console.log("fuuuuck");
           }
-          console.log("YYYAAAAAS");
+          else {
+            console.log("YYYAAAAAS");
+          }
         })
   });
-
-
 }
+
 //create a user
 exports.createUser = function(req, response) {
   const userID = req.body.id;
   //console.log("req.body:",req.body.id); //DEBUG
-   
-  db.query('INSERT INTO users (uid) VALUES($1)', [userID], 
+
+  db.query('INSERT INTO users (uid) VALUES($1)', [userID],
     (err, res) => {
       if (err) {
         res.status(404)        // HTTP status 404: Not Found
@@ -82,9 +83,10 @@ exports.createUser = function(req, response) {
         console.log('Error while trying to create user');
         throw err;
       }
-      console.log('this is what server controller is returning: ' + res.rows[0]);
-      res.send('User', userID, 'added to database'); // untested
-      return response.json(res.rows[0]);
+      else {
+        console.log('this is what server controller is returning: ' + res.rows[0]);
+        //res.send('User', userID, 'added to database'); // untested
+        return response.json(res.rows[0]);
+      }
     })
-    
 };
